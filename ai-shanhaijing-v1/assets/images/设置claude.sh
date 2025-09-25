f0ab5a826706ea1a4c2de331b32110bcc3765a8e
weibinqiu <domiri@foxmail.com>
2025-09-25 22:24:23 +0800

    提交v1版本修改

---
+++ ai-shanhaijing-v1/assets/images/设置claude.sh
@@ -0,0 +1,9 @@
+
+echo -e '\n export ANTHROPIC_AUTH_TOKEN=ee01af64877d42e28ab0c5868b5ac73b.ZvP13GiR33XDJQ1i' >> ~/.bash_profile
+echo -e '\n export ANTHROPIC_BASE_URL=https://open.bigmodel.cn/api/anthropic' >> ~/.bash_profile
+
+echo -e '\n export ANTHROPIC_AUTH_TOKEN=ee01af64877d42e28ab0c5868b5ac73b.ZvP13GiR33XDJQ1i' >> ~/.bashrc
+echo -e '\n export ANTHROPIC_BASE_URL=https://open.bigmodel.cn/api/anthropic' >> ~/.bashrc
+
+echo -e '\n export ANTHROPIC_AUTH_TOKEN=ee01af64877d42e28ab0c5868b5ac73b.ZvP13GiR33XDJQ1i' >> ~/.zshrc
+echo -e '\n export ANTHROPIC_BASE_URL=https://open.bigmodel.cn/api/anthropic' >> ~/.zshrc
